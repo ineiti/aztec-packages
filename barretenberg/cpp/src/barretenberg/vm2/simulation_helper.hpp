1eb3bdf0b05fc6f2216b14fd159c42393d4d6d1e
MirandaWood <miranda@aztecprotocol.com>
2025-11-04 17:33:02 +0000

    chore: comments, renaming

--- barretenberg/cpp/src/barretenberg/vm2/simulation_helper.hpp
+++ barretenberg/cpp/src/barretenberg/vm2/simulation_helper.hpp
@@ -22,8 +22,8 @@ class AvmSimulationHelper {
                                                       const ProtocolContracts& protocol_contracts);
 
     TxSimulationResult simulate_fast_with_hinted_dbs(const ExecutionHints& hints);
-    // Note: we currently only have hinted raw dbs, TODO eventually remove hints:
-    TxSimulationResult simulate_fast_without_hinted_dbs(const ExecutionHints& hints);
+    // Note: we currently only have hinted raw dbs, TODO eventually use real dbs and remove hint inputs:
+    TxSimulationResult simulate_fast_with_real_dbs(const ExecutionHints& hints);
 
     // Simulate a bytecode with some calldata and additional context.
     // Note: this assumes that no nested calls are ever made to other bytecodes.
